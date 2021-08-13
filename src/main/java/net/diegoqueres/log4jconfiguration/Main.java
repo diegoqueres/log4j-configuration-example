6df0ad6244f2519fa9f9b836978a29a29aff0877
Diego Queres <diego.queres@stoom.com.br>
2021-08-13 10:12:03 -0300

    Usando ThreadContext NDC para identificar contexto do log: usuário, sessão, origem, etc

--- src/main/java/net/diegoqueres/log4jconfiguration/Main.java
+++ src/main/java/net/diegoqueres/log4jconfiguration/Main.java
@@ -2,11 +2,14 @@ package net.diegoqueres.log4jconfiguration;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.ThreadContext;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        initNdcConfiguration();
+
         String message = "Hello there!";
         System.out.println(message);
         logger.debug(message);
@@ -14,6 +17,22 @@ public class Main {
         logger.error(message);
 
         LoggerChild.log();
+
+        finishNdcConfiguration();
+    }
+
+    static void initNdcConfiguration() {
+        //Irá prover uma saída com identificação, como o exemplo: 10:04:35.097 [main] [admin, 1234] ERROR com.company.Main ...
+        String username = "admin";
+        String sessionId = "1234";
+
+        ThreadContext.push(username);
+        ThreadContext.push(sessionId);
+    }
+
+    static void finishNdcConfiguration() {
+        ThreadContext.pop();
+        ThreadContext.removeStack();
     }
 
     private static class LoggerChild {
